89df60cc30632241631e3adbc22a0a4b03ec4e28
MichelleBaird-DBT <108753201+MichelleBaird-DBT@users.noreply.github.com>
2022-08-08 13:00:03 -0400

    Add files via upload

---
+++ randy_pitcher_sales_demos-production/macros/config/generate_schema_name.sql
@@ -0,0 +1,22 @@
+{%- macro generate_schema_name(custom_schema_name, node) -%}
+
+    {% if target.name == 'default' %}
+        {{target.schema}}{{ '_' ~ custom_schema_name if custom_schema_name else '' }}
+
+    {% elif target.name == 'pr_testing' %}
+        {{target.schema}}{{ '_' ~ custom_schema_name if custom_schema_name else '' }}   
+
+    {% elif 'production' in target.name %}
+        {{ custom_schema_name if custom_schema_name else target.schema }}
+        
+    {% else %}
+        {%- set err_msg -%}
+            Unrecognized target.name provided - Got: {{target.name}}
+                node.identifier: {{node.identifier}}
+                custom_schema_name: {{custom_schema_name}}
+                node: {{node}}
+        {%- endset -%}
+        {{ exceptions.raise_compiler_error(err_msg) }}
+    {% endif %}
+
+{%- endmacro -%}
