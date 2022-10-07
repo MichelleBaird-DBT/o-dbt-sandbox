51eae4add9094bc58801e63ad97bb6b3bf538b7b
Michelle Baird <michelle.baird@fishtownanalytics.com>
2022-10-07 18:18:09 +0000

    adsfads

--- models/retail/marts/monthly_gross_revenue.sql
+++ models/retail/marts/monthly_gross_revenue.sql
@@ -1,13 +1,14 @@
 
 select
     date_trunc(month, order_date) as order_month,
+    region as region,
     sum(gross_item_sales_amount)  as gross_revenue
 
 from 
     {{ref('fct_order_items')}}
 
 group by 
-    order_month
+    order_month, region
 
 order by 
     order_month
